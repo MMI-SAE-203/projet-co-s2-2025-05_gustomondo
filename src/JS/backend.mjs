cb3448ec85a3268f37b47c505eeea838bfe2447f
Enzo Locatelli <enzort70290@gmail.com>
2025-06-05 18:28:54 +0200

    test-VPS

--- src/JS/backend.mjs
+++ src/JS/backend.mjs
@@ -2,7 +2,7 @@
 
 import PocketBase from 'pocketbase';
 
-const pb = new PocketBase('http://127.0.0.1:8090');
+const pb = new PocketBase("https://gustomundo.lucas-lebars.fr:443");
 
 pb.autoCancellation(false);
 
